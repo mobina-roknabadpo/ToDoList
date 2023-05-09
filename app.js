2d537f9c550347817686d361ed6168a7b41ffa12
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-05-09 17:38:56 +0330

    feat: create new element in js file

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const buttons = document.querySelector(".property").querySelectorAll("button");
 const task = document.getElementById('task');
 const des = document.getElementById('description');
 const date = document.getElementById('date');
@@ -9,23 +8,39 @@ const display = document.querySelector('.display');
 (function () {
     submit.addEventListener('submit', (e) => {
         e.preventDefault();
-        const div = document.createElement('div');
-        div.className = "on";
+        const divParent = document.createElement('div');
+        divParent.className = "list";
+        const divChild = document.createElement('div');
+        divChild.className = "on";
+        //create element with task name
         const header1 = document.createElement('h3');
         header1.innerHTML = `<span style="color: #2d67b7">Task Name:</span> ${task.value}`;
+        //create element with description
         const header2 = document.createElement('h3');
         header2.innerHTML = `<span style="color: #2d67b7">Description:</span> ${des.value}`;
+        //create element with date
         const header3 = document.createElement('h3');
         let newDate = new Date(date.value);
         header3.innerHTML = `<span style="color: #2d67b7">Date:</span> ${[newDate.getDate(), newDate.getMonth(), newDate.getFullYear()].join('-')}`;
+        //create element with days
         const header4 = document.createElement('h3');
         const days = ["SUN", "MON", "TUE", "WEN", "THU", "FRI", "SAT"];
         header4.innerHTML = `<span style="color: #2d67b7; font-size: 1.1rem">Day:</span> ${days[newDate.getDay()]}`;
-        div.appendChild(header1);
-        div.appendChild(header2);
-        div.appendChild(header3);
-        div.appendChild(header4);
-        showItem.appendChild(div);
+        divChild.appendChild(header1);
+        divChild.appendChild(header2);
+        divChild.appendChild(header3);
+        divChild.appendChild(header4);
+        divParent.appendChild(divChild);
+        console.log(divParent)
+        const trashIcon = document.createElement('i');
+        trashIcon.className = "fa-solid fa-trash";
+        trashIcon.id = "trash";
+        trashIcon.style.color = '#3a5a9b';
+        trashIcon.addEventListener('click', () => {
+            divParent.remove();
+        })
+        divParent.appendChild(trashIcon);
+        showItem.appendChild(divParent);
         //after add tp do list empty fields
         task.value = "";
         des.value = "";
@@ -52,66 +67,51 @@ const display = document.querySelector('.display');
         }
     })
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            if (button.className === 'max') {
-                max();
-            } else if (button.className === 'min') {
-                min();
-            } else if (button.className === 'average') {
-                average();
-            } else if (button.className === 'sort') {
-                sort();
-            } else if (button.className === 'clear') {
-                clear();
-            }
-        })
-    })
 
-    const max = () => {
-        const maximum = [];
-        const div = showItem.querySelectorAll('div');
-        div.forEach(item => {
-            let age = +item.getElementsByTagName('h3')[1].textContent;
-            maximum.push(age);
-        })
-        display.innerHTML = `Maximum: ${Math.max(...maximum)}`;
-    }
-
-    const min = () => {
-        const minimum = [];
-        const div = showItem.querySelectorAll('div');
-        div.forEach(item => {
-            let age = +item.getElementsByTagName('h3')[1].textContent;
-            minimum.push(age);
-        })
-        console.log(minimum)
-        display.innerHTML = `Minimum: ${Math.min(...minimum)}`;
-    }
-
-    const average = () => {
-        let count = 0;
-        let sum = 0;
-        const div = showItem.querySelectorAll('div');
-        div.forEach(item => {
-            sum += +item.getElementsByTagName('h3')[1].textContent;
-            count++;
-        })
-        let avg = sum / count;
-        if (!isNaN(avg)) {
-            display.innerHTML = `Average: ${Math.floor(avg)}`;
-        }
-    }
-
-    const sort = () => {
-        const div = showItem.querySelectorAll('div');
-
-    }
-
-    const clear = () => {
-        showItem.innerHTML = "";
-        display.innerHTML = "";
-    }
+    // const max = () => {
+    //     const maximum = [];
+    //     const div = showItem.querySelectorAll('div');
+    //     div.forEach(item => {
+    //         let age = +item.getElementsByTagName('h3')[1].textContent;
+    //         maximum.push(age);
+    //     })
+    //     display.innerHTML = `Maximum: ${Math.max(...maximum)}`;
+    // }
+    //
+    // const min = () => {
+    //     const minimum = [];
+    //     const div = showItem.querySelectorAll('div');
+    //     div.forEach(item => {
+    //         let age = +item.getElementsByTagName('h3')[1].textContent;
+    //         minimum.push(age);
+    //     })
+    //     console.log(minimum)
+    //     display.innerHTML = `Minimum: ${Math.min(...minimum)}`;
+    // }
+    //
+    // const average = () => {
+    //     let count = 0;
+    //     let sum = 0;
+    //     const div = showItem.querySelectorAll('div');
+    //     div.forEach(item => {
+    //         sum += +item.getElementsByTagName('h3')[1].textContent;
+    //         count++;
+    //     })
+    //     let avg = sum / count;
+    //     if (!isNaN(avg)) {
+    //         display.innerHTML = `Average: ${Math.floor(avg)}`;
+    //     }
+    // }
+    //
+    // const sort = () => {
+    //     const div = showItem.querySelectorAll('div');
+    //
+    // }
+    //
+    // const clear = () => {
+    //     showItem.innerHTML = "";
+    //     display.innerHTML = "";
+    // }
 
     const hamburger = document.querySelector('.hamburger');
     const sidebar = document.querySelector('.sidebar');
