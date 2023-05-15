69357e4f9cb2e8bf523f9e0c8381a6428be33f48
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-05-15 16:46:29 +0330

    change: rewrite

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 const task = document.getElementById('task');
 const des = document.getElementById('description');
 const date = document.getElementById('date');
-let input = document.getElementById('search');
+const input = document.getElementById('search');
 const showItem = document.querySelector('.show-item');
 const submit = document.getElementById('form');
 (function () {
@@ -11,35 +11,23 @@ const submit = document.getElementById('form');
         divParent.className = "list";
         const divChild = document.createElement('div');
         divChild.className = "on";
-        //create element with task name
-        const header1 = document.createElement('h3');
-        header1.innerHTML = `<span style="color: #2d67b7">Task Name:</span> ${task.value}`;
-        //create element with description
-        const header2 = document.createElement('h3');
-        header2.innerHTML = `<span style="color: #2d67b7">Description:</span> ${des.value}`;
-        //create element with date
-        const header3 = document.createElement('h3');
         let newDate = new Date(date.value);
-        header3.innerHTML = `<span style="color: #2d67b7">Date:</span> ${[newDate.getDate(), newDate.getMonth(), newDate.getFullYear()].join('-')}`;
-        //create element with days
-        const header4 = document.createElement('h3');
         const days = ["SUN", "MON", "TUE", "WEN", "THU", "FRI", "SAT"];
-        header4.innerHTML = `<span style="color: #2d67b7; font-size: 1.1rem">Day:</span> ${days[newDate.getDay()]}`;
-        divChild.appendChild(header1);
-        divChild.appendChild(header2);
-        divChild.appendChild(header3);
-        divChild.appendChild(header4);
-        divParent.appendChild(divChild);
-        console.log(divParent)
+        divChild.innerHTML = `<h3> <span style="color: #2d67b7">Task Name:</span> ${task.value}</h3>
+<h3><span style="color: #2d67b7">Description:</span> ${des.value}</h3>
+<h3><span style="color: #2d67b7">Date:</span> ${[newDate.getDate(), newDate.getMonth(), newDate.getFullYear()].join('-')}</h3>
+<h3><span style="color: #2d67b7; font-size: 1.1rem">Day:</span> ${days[newDate.getDay()]}</h3>
+`;
         const trashIcon = document.createElement('i');
         trashIcon.className = "fa-solid fa-trash";
-        trashIcon.id = "trash";
         trashIcon.style.color = '#3a5a9b';
         trashIcon.addEventListener('click', () => {
             divParent.remove();
         })
+        divParent.appendChild(divChild);
         divParent.appendChild(trashIcon);
         showItem.appendChild(divParent);
+
         //after add tp do list empty fields
         task.value = "";
         des.value = "";
